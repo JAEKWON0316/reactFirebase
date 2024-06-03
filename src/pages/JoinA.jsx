6f5003a5fb4788154f5a960df0c0f13b62bc5e94
JK Lee <diwo02@naver.com>
2024-06-03 18:13:47 +0900

    firebase와 챗룸작업중

--- src/pages/JoinA.jsx
+++ src/pages/JoinA.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import { FaUserLarge } from "react-icons/fa6";
 import { Link, useNavigate } from 'react-router-dom'
 import { Alert}  from 'react-bootstrap';
-import { auto } from '../config/firebase'
+import { auth } from '../config/firebase'
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 const JoinA = () => {
@@ -23,34 +23,46 @@ const JoinA = () => {
   
   //검증식
   const validate = (values) => {
+    let formError = true;
     const regex = /[a-zA-Z0-9._+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9.]+/i; //모든영문자 숫자 가능 그리고 + @꼭있어야한다  
 
     if(!values.email){
+      formError = false;
       setError("이메일 주소를 입력 하세요.");
       setShow(true);
     }
     else if(!regex.test(values.email)){ //정규식 검증할 때 test를 써주면 된다.
+      formError = false;
       setError("이메일 형식이 아닙니다.");
       setShow(true);
     }
     else if(!values.pass){
+      formError = false;
       setError("비밀번호를 입력하세요.");
       setShow(true);
     }
     else if(values.pass.length < 4){
+      formError = false;
       setError("비밀번호는 4자 이상이어야 합니다.");
       setShow(true);
     }
     else if(values.pass !== values.repass){ //react 에서는 === !== 이런식으로 3개를 써줘야함.
+      formError = false;
       setError("비밀번호가 일치하지 않습니다.");
       setShow(true);
     }
-
+    return formError;
   }
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     if(validate(formValues)){
-
+      try{
+        await createUserWithEmailAndPassword(auth, formValues.email, formValues.pass);
+        nav('/joinb');
+      }
+      catch(error){
+        console.error('에러가 발생했습니다.', error);
+      }
     }
   }
 
@@ -72,7 +84,7 @@ const JoinA = () => {
               <span><FaUserLarge /></span>
               <h1 className='mt-3'>회원가입 1단계</h1>
             </div>
-            <form className="login-form">
+            <form className="login-form" method='post'>
               <input type="text" 
                       className='form-control' 
                       placeholder='이메일'
@@ -94,8 +106,7 @@ const JoinA = () => {
                       />
               <button type='submit' 
                       className='btn btn-primary rounded submit'
-                      onClick={handleSubmit}  
-                      >다음단계</button>
+                      onClick={handleSubmit}>다음단계</button>
               <Link to="/" className='btn btn-link link'>회원로그인</Link>
             </form>
           </div>
