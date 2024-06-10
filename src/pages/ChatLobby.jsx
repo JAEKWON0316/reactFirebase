7474d5a58cc07bc5bdab2d0280ecd75d579c37d1
JK Lee <diwo02@naver.com>
2024-06-10 16:27:35 +0900

    채팅방 작업

--- src/pages/ChatLobby.jsx
+++ src/pages/ChatLobby.jsx
@@ -1,60 +1,107 @@
 import React, {useState, useEffect} from 'react'
-import { useNavigate, Link } from 'react-router-dom'
+import { useNavigate, Link} from 'react-router-dom'
 import { useAuthValue } from '../context/AuthProvider'
-import { db, auth } from '../config/firebase'  //firebase 데이터베이스 변수 받아오기
-import { addDoc, collection, query, orderBy, onSnapshot, serverTimestamp } from 'firebase/firestore';
-import { signOut } from 'firebase/auth';
+import { db, auth } from '../config/firebase'
+import { addDoc, 
+         collection, 
+         query, 
+         orderBy, 
+         onSnapshot, 
+         where,
+         serverTimestamp, 
+         deleteDoc,
+         getDocs,
+         doc,
+         setDoc
+      } from 'firebase/firestore';
+import { signOut, getAuth, onAuthStateChanged } from 'firebase/auth'
 
-const ChatLobby = () => {   //provider에서 바로 뿌려주는것이다. 
+const ChatLobby = () => {
   const [roomname, setRoomname] = useState("");
-  const [roomList, setRoomList] = useState([]); //useState 배열 타입이다. []은 배열 {}는 오브젝트
+  const [roomList, setRoomList] = useState([]);
   const { nick, userIcon, email } = useAuthValue();
   const nav = useNavigate();
+  const user = auth.currentUser;  //로그인한 사용자 정보
 
+  /*
+  onAuthStateChanged( auth , user => { 
+     const userRef = doc(db, "activeUsers", user.uid);
+     if(user){ //로그인 상태 - activeUser에다 로그인된 회원정보를 등록  
+       setDoc(userRef, {
+           uid: user.uid,
+           email: user.email,
+           nick: user.displayName,
+           uicon: user.photoURL,
+           lastLogin: new Date()
+       }, { merge: true})
+       .catch(error=> console.error("에러가 발생했습니다.", error));
+     }else{  //로그아웃 상태 또는 삭제 - activeUser에 등록된 정보를 삭제
+        deleteDoc(userRef)
+       .catch(error=> console.error("에러가 발생했습니다.", error));
+     }
+  });
+  */
+ 
   const logout = () => {
-    signOut(auth)
-      .then(()=>{  //promise와 똑같다.
-        nav("/login");
-      })
-      .catch((err) => console.error('로그아웃하다가 삑사리', err));
+     signOut(auth)
+       .then(()=>{
+          nav("/login");
+       })
+       .catch((err) => console.error('로그아웃하다가 삑사리', err));
   }
-  
+
+  //방장이름이 null값인 경우 채팅방을 삭제
+  const handleDelRoom = async() => {
+     try{
+        const dbref = collection(db, "chatroom");
+        const sql = query(dbref, where("master", "==", null));
+        const querySnapshot = await getDocs(sql);
+        querySnapshot.forEach(async (doc) => {
+            await deleteDoc(doc.ref);  //루프를 돌며 master가 null인 채팅룸 삭제
+        });
+     }catch(error){
+        console.error("에러가 발생했습니다.", error);
+     }  
+  }
+
   //db의 chatroom에서 읽기
   const getRoom = () => {
-    const sql = query(collection(db, 'chatroom'), orderBy("timestamp", "desc"));
-    onSnapshot(sql, (res) => {
-      const rooms = res.docs.map(doc => ({
-        ...doc.data()
-      }));
-      setRoomList(rooms);
-    });
+     const sql = query(collection(db, 'chatroom'), orderBy("timestamp", "desc"));
+     onSnapshot(sql, (res) => {
+        const rooms =res.docs.map(doc => ({
+            id: doc.id,
+            ...doc.data()
+        }));
+        setRoomList(rooms);
+     });
   }
-
+ 
   useEffect(()=>{
-    getRoom();
+     handleDelRoom();
+     getRoom();
   }, []);
 
-  //db의 chatroom이라는 테이블에 쓰기(insert)
+  //db의 chatroom이라는 테이블에 쓰기
   const handleMakeRoom = async (e) => {
-    e.preventDefault();
-    const dbref = collection(db, 'chatroom');
-      await addDoc(dbref,{
-        timestamp: serverTimestamp(),
-        title: roomname,
-        master:nick,
-        email
-      });
-      setRoomname("");
+     e.preventDefault();
+     const dbref = collection(db, 'chatroom');
+        await addDoc(dbref, {
+           timestamp: serverTimestamp(),
+           title: roomname,
+           master: nick,
+           email
+        });
+        setRoomname("");
   }
 
   return (
     <div className="container">
-       <div className="header text-center">
+       <div className="header text-center mt-5 pt-3">
            <img src={userIcon} alt={nick} className="usericon"/>
-           <h2 className="text-center">{nick}님 환영합니다.</h2>
-           <button type="button" onClick={logout} className="btn btn-warning">로그아웃</button>
+           <h3 className="text-center mt-4"><strong>{nick}</strong>님 환영합니다.</h3>
+           <button type="button" onClick={logout} className="btn btn-warning mt-3">로그아웃</button>
        </div>
-       <form className="makechat my-4" method="post" onSubmit={handleMakeRoom}>
+       <form className="makechat my-5" method="post" onSubmit={handleMakeRoom}>
            <h2 className="text-center">채팅방개설</h2>
            <input 
                type="text"
@@ -67,21 +114,19 @@ const ChatLobby = () => {   //provider에서 바로 뿌려주는것이다.
                    className="btn btn-primary"
             >채팅방만들기</button>           
        </form>
-
        <div className="row">
-        {
-          (roomList) &&
-            roomList.map((rs, index) => (
-              <div className="col-3" key={index}> {/*반복되는 부분은 반드시 key를 만들어서 따로따로 구분되게 해야한다.*/}
-                <Link to={`/chat/${rs.id}`}>{rs.title}</Link>
-                <p>(방장: {rs.master}님)</p>
-              </div>
-            ))
-        }
+           {
+              (roomList) && 
+                 roomList.map((rs, index)=>(
+                    <div className="chatroom col-3" key={index}>
+                       <Link to={`/chat/${rs.id}`}>{rs.title}</Link>
+                       <p>(방장 : {rs.master}님)</p>
+                    </div>  
+                 ))
+           }
        </div>
     </div>
   )
 }
-
 
 export default ChatLobby
--- src/pages/ChatRoom.jsx
+++ src/pages/ChatRoom.jsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom'
+import { query, updateDoc, getDocs, addDoc,collection, where, doc, increment } from 'firebase/firestore'
+import { db } from '../config/firebase'
+import { getAuth } from 'firebase/auth'
 
 const ChatRoom = () => {
+  const { id:roomId } = useParams();
+  const auth = getAuth();
+  const [user, setUser] = useState(auth.currentUser);
+
+  useEffect(()=>{
+    const unsubscribe = auth.onAuthStateChanged(setUser);
+    return ()=>unsubscribe();
+  }, [auth]);
+
+  const enterChatRoom = async() => {
+     if(!roomId || !user) return;
+     const qry = query(
+                   collection(db, "mchat"), 
+                   where("roomId", "==", roomId),
+                   where("userId", "==", user.uid));
+     const snapshot = await getDocs(qry);               
+    
+     if(snapshot.empty){ //이미 정보가 등록되지 않은 경우에만 업데이트
+       //chatroom에 회원수 1증가
+       const roomRef = doc(db, 'chatroom', roomId);
+       await updateDoc(roomRef, {
+           memcount: increment(1)
+       });
+
+       //mchat에 대화방 참가자 추가
+       const mchatRef = collection(db, 'mchat');
+       await addDoc(mchatRef, {
+          userid: user.uid,
+          roomId: roomId,
+          nick: user.displayName,
+          email: user.email,
+          uicon: user.photoURL
+       })
+
+       //채팅방 입장
+       const chatsRef = collection(db, 'chats');
+       await addDoc(chatsRef, {
+           roomId: roomId,
+           message: `${user.displayName}님이 입장하셨습니다.`,
+           timestamp: new Date()
+       })
+     }              
+  }
+  
+  useEffect(()=>{
+     enterChatRoom();
+  }, [roomId, user, enterChatRoom]);
+
   return (
-    <div>ChatRoom</div>
+    <div>{user.displayName}</div>
   )
 }
 
